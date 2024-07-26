0ab4bfd4a3dae8f6391c776afb85a54dc8374424
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-07-26 10:43:07 +0300

    Initial commit

---
+++ src/main/java/org/training/turkcell/kafka/Kafka20240726Application.java
@@ -0,0 +1,14 @@
+package org.training.turkcell.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka20240726Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kafka20240726Application.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/org/training/turkcell/kafka/Kafka20240726ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.training.turkcell.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kafka20240726ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
